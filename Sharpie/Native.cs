096e8fa483f881fb344d516cc42cae85466d3bec
Philip Ma <philipma92@gmail.com>
2014-05-07 00:15:18 -0400

    NativeEWF Update
    Implementation of ewfapi

--- Sharpie/Native.cs
+++ Sharpie/Native.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -9,8 +9,17 @@ using System.Windows.Forms;
 
 namespace Sharpie
 {
-    class Native
+    public class Native
     {
+        public enum EWX_Flags
+        {
+            LOGOFF = 0,
+            POWEROFF = 0x00000008,
+            REBOOT = 0x00000002,
+            SHUTDOWN = 0x00000001,
+            FORCE = 0x00000004,
+            FORCEIFHUNG = 0x00000010
+        }
         [DllImport("user32.dll")]
         public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
         [DllImport("user32.dll")]
@@ -19,6 +28,8 @@ namespace Sharpie
         static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, UIntPtr dwExtraInfo);
         [DllImportAttribute("User32.dll")]
         public static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImportAttribute("User32.dll")]
+        public static extern bool ExitWindowsEx(EWX_Flags uFlags, string dwReason);
         [DllImport("kernel32.dll")]
         public static extern bool WriteProcessMemory(IntPtr hProcess, UIntPtr lpBaseAddress, byte[] lpBuffer, UIntPtr nSize, IntPtr lpNumberOfBytesWritten);
         [DllImport("kernel32.dll")]
@@ -27,7 +38,27 @@ namespace Sharpie
         public static extern bool _CloseHandle(IntPtr hObject);
         [DllImport("kernel32.dll")]
         public static extern bool ReadProcessMemory(IntPtr hProcess, UIntPtr lpBaseAddress, [Out] byte[] lpBuffer, UIntPtr nSize, IntPtr lpNumberOfBytesRead);
+        [DllImport("Kernel32.dll")]
+        public static extern IntPtr LoadLibrary(string lpFileName);
+        [DllImport("Kernel32.dll")]
+        public static extern bool FreeLibrary(IntPtr hModule);
+        [DllImport("Kernel32.dll")]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+        [DllImport("Kernel32.dll")]
+        public static extern bool SetDllDirectory(string lpPathName);
+        [DllImport("Kernel32.dll")]
+        public static extern string GetLastError();
 
+
+        public static Delegate LoadFunction<T>(string dllPath, string functionName)
+        {
+            IntPtr hModule = LoadLibrary(dllPath);
+            IntPtr address = GetProcAddress(hModule, functionName);
+            return Marshal.GetDelegateForFunctionPointer(address, typeof(T));
+        }
+
+
+        
         public static void clickAtPoint(Point p)
         {
             Cursor.Position = p;
---
+++ Sharpie/NativeEWF.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpie
+{
+    public unsafe struct EWF_Volume_Name_Entry
+    {
+        public EWF_Volume_Name_Entry* Next;
+        public char Name;
+    }
+
+    public unsafe class NativeEWF
+    {
+        
+        [DllImport("Ewfapi.dll")]
+        public static extern IntPtr EwfMgrOpenProtected(string lpVolume);
+        [DllImport("Ewfapi.dll")]
+        public static extern bool EwfMgrCommit(IntPtr hDevice);
+        [DllImport("Ewfapi.dll")]
+        public static extern bool EwfMgrClose(IntPtr hDevice);
+        [DllImport("Ewfapi.dll")]
+        public static extern bool EwfMgrEnable(IntPtr hDevice);
+        [DllImport("Ewfapi.dll")]
+        public static extern bool EwfMgrDisable(IntPtr hDevice, bool fCommit);
+        [DllImport("Ewfapi.dll")]
+        public static extern bool EwfMgrVolumeNameListIsEmpty(EWF_Volume_Name_Entry* pVolumeNameList);
+        [DllImport("Ewfapi.dll")]
+        public static extern EWF_Volume_Name_Entry* EwfMgrGetProtectedVolumeList();
+        [DllImport("Ewfapi.dll")]
+        public static extern char EwfMgrGetDriveLetterFromVolumeName(string lpcwStr);
+        [DllImport("Ewfapi.dll")]
+        public static extern void EwfMgrVolumeNameEntryPop(EWF_Volume_Name_Entry** ppVolumeNameList);
+        [DllImport("Ewfapi.dll")]
+        public static void EwfMgrVolumeNameListDelete(EWF_Volume_Name_Entry* pVolumeNameList);
+
+        public static string Commit(string volumeName, bool reboot)
+        {
+            IntPtr hDevice = EwfMgrOpenProtected(volumeName);
+            if (hDevice.ToInt32() == -1)
+            {
+                goto exit;
+            }
+
+            if (!EwfMgrCommit(hDevice))
+            {
+                goto exit;
+            }
+
+            if (reboot) Reboot();
+
+        exit:
+            if (hDevice.ToInt32() != -1)
+                EwfMgrClose(hDevice);
+            return Native.GetLastError();
+        }
+
+        public static string Enable(string volumeName, bool reboot)
+        {
+            IntPtr hDevice = EwfMgrOpenProtected(volumeName);
+            if (hDevice.ToInt32() == -1)
+            {
+                goto exit;
+            }
+
+            if (!EwfMgrEnable(hDevice))
+            {
+                goto exit;
+            }
+
+            if (reboot) Reboot();
+
+        exit:
+            if (hDevice.ToInt32() != -1)
+                EwfMgrClose(hDevice);
+            return Native.GetLastError();
+        }
+
+        public static string Disable(string volumeName, bool reboot)
+        {
+            IntPtr hDevice = EwfMgrOpenProtected(volumeName);
+            if (hDevice.ToInt32() == -1)
+            {
+                goto exit;
+            }
+
+            if (!EwfMgrDisable(hDevice, false))
+            {
+                goto exit;
+            }
+
+            if (reboot) Reboot();
+
+        exit:
+            if (hDevice.ToInt32() != -1)
+                EwfMgrClose(hDevice);
+            return Native.GetLastError();
+        }
+
+
+        public static void Reboot()
+        {
+            Native.ExitWindowsEx(Native.EWX_Flags.REBOOT | Native.EWX_Flags.FORCE, "");
+        }
+
+        public unsafe static List<char> GetProtectedVolumeList()
+        {
+            EWF_Volume_Name_Entry* volumeNames = EwfMgrGetProtectedVolumeList();
+            List<char> protectedVolumes = new List<char>();
+            while (!EwfMgrVolumeNameListIsEmpty(volumeNames))
+            {
+                IntPtr hDevice = EwfMgrOpenProtected(volumeNames->Name.ToString());
+                if (hDevice.ToInt32() != -1)
+                {
+                    protectedVolumes.Add(volumeNames->Name);
+                    EwfMgrClose(hDevice);
+                }
+                hDevice = IntPtr.Zero;
+                EwfMgrVolumeNameEntryPop(&volumeNames);
+            }
+            EwfMgrVolumeNameListDelete(volumeNames);
+            return protectedVolumes;
+
+        }
+    }
+}
