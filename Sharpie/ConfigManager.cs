95a04f897ed87d28669a353070e49c1fb431e699
Philip Ma <philipma92@gmail.com>
2015-04-15 18:44:31 -0400

    ConfigManager
    ConfigManager contains functions that assists in loading the app.config
    file part of C# projects
    
    Functions:
    ReadAllSettings - Reads all app setting key and value and returns a list
    of tuples<string, string>
    ReadSettings - Returns a value given a known key name
    LoadConfig - Loads an object's variables with the same variable name as
    the key name and assigns the variable the value from the app.config file
    that is part of c# projects

---
+++ Sharpie/ConfigManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpie
+{
+    class ConfigManager
+    {
+        static List<Tuple<string, string>> ReadAllSettings()
+        {
+            if (System.Configuration.ConfigurationManager.AppSettings.Count == 0)
+            {
+                return null;
+            }
+
+            List<Tuple<string, string>> settings = new List<Tuple<string, string>>();
+
+            foreach (string key in System.Configuration.ConfigurationManager.AppSettings.AllKeys)
+            {
+                settings.Add(new Tuple<string, string>(key, System.Configuration.ConfigurationManager.AppSettings[key]));
+            }
+            return settings;
+        }
+
+        static string ReadSettings(string key)
+        {
+            return System.Configuration.ConfigurationManager.AppSettings[key] ?? null;
+        }
+
+        public static dynamic LoadConfig(dynamic o)
+        {
+            o = Activator.CreateInstance(o.GetType());
+            List<Tuple<string, string>> settings = ReadAllSettings();
+            foreach (Tuple<string, string> s in settings)
+            {
+                o.GetType().GetField(s.Item1.ToString().Trim()).SetValue(o, s.Item2);
+            }
+            return o;
+        }
+    }
+}
